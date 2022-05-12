66ae367abe1ac08705f88248f6993fd21aca9963
Matthew Tripodi <mtripodi1027@gmail.com>
2022-05-11 21:04:27 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Matthew Tripodi on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
